71e2de759f2c3418e2b23105b504ec77d9b5c871
Fumiya Oura <fumiya.oura.s8@dc.tohoku.ac.jp>
2024-06-04 02:32:42 +0900

    1st commit from fe_easiroc01

--- Frontend/optlink_core/Header.cc
+++ Frontend/optlink_core/Header.cc
@@ -1,1 +1,44 @@
-../vme_xvb_core/Header.cc
+// -*- C++ -*-
+
+// Author: Shuhei Hayakawa
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "Header.hh"
+
+namespace vme
+{
+
+//____________________________________________________________________________
+void
+SetMasterHeader( dtype32  data_size,
+		 dtype32  nblock,
+		 dtype32* position )
+{
+  MasterHeader vme_master_header;
+  vme_master_header.m_magic     = MasterMagic;
+  vme_master_header.m_data_size = data_size;
+  vme_master_header.m_nblock    = nblock;
+  std::memcpy( position, &vme_master_header, MasterHeaderSize*4 );
+}
+
+//____________________________________________________________________________
+void
+SetModuleHeader( dtype64  vme_address,
+		 dtype64  data_size,
+		 dtype32* ptr )
+{
+  ModuleHeader vme_module_header;
+  vme_module_header.m_magic               = ModuleMagic;
+  vme_module_header.m_vme_address         = vme_address;
+  vme_module_header.m_data_size           = data_size;
+  vme_module_header.m_n_times_read_device = 0;
+  vme_module_header.m_module_type[0]      = 0;
+  vme_module_header.m_module_type[1]      = 0;
+  vme_module_header.m_tv_sec              = 0;
+  vme_module_header.m_tv_nsec             = 0;
+  std::memcpy( ptr, &vme_module_header, ModuleHeaderSize*4 );
+}
+
+}
--- Frontend/optlink_core/Header.hh
+++ Frontend/optlink_core/Header.hh
@@ -1,1 +1,69 @@
-../vme_xvb_core/Header.hh
+// -*- C++ -*-
+
+// Author: Shuhei Hayakawa
+
+#ifndef VME_HEADER_HH
+#define VME_HEADER_HH
+
+#include <stdint.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <byteswap.h>
+
+#ifdef fOpticalLink
+
+ typedef uint32_t dtype32;
+ typedef uint64_t dtype64;
+
+#else
+
+ #include <gef/gefcmn_vme.h>
+ typedef GEF_UINT32 dtype32;
+ typedef GEF_UINT64 dtype64;
+
+#endif
+
+
+namespace vme
+{
+
+//____________________________________________________________________________
+/* VME */
+struct MasterHeader
+{
+  dtype32  m_magic;
+  dtype32  m_data_size;
+  dtype32  m_nblock;
+};
+const dtype32     MasterMagic      = 0x00564d45U;
+const std::size_t MasterHeaderSize = sizeof(MasterHeader)/sizeof(dtype32);
+
+//____________________________________________________________________________
+/* VMEBOARD */
+struct ModuleHeader
+{
+  dtype64 m_magic;
+  dtype64 m_vme_address;
+  dtype64 m_data_size;
+  dtype64 m_n_times_read_device;
+  dtype64 m_module_type[2];
+  dtype64 m_tv_sec;
+  dtype64 m_tv_nsec;
+};
+const dtype64     ModuleMagic      = 0x766d65626f617264ULL;
+const std::size_t ModuleHeaderSize = sizeof(ModuleHeader)/sizeof(dtype32);
+
+//____________________________________________________________________________
+void
+SetMasterHeader( dtype32  data_size,
+		 dtype32  nblock,
+		 dtype32* position );
+void
+SetModuleHeader( dtype64  vme_address,
+		 dtype64  data_size,
+		 dtype32* position );
+
+}
+
+#endif
